3268cb828adade50bce1f0941e4a33a75f2d2113
Matthew Wright <matthewbwright@gmail.com>
2021-10-16 14:17:56 -0700

    creates a constructor to accept values

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,14 @@
 package com.h2;
 
 public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
 
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
 }
